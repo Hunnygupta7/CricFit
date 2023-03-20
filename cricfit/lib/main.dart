338e198d6612a35b0317fdf65bec8330d5ad98d4
Harvinder-Singh24 <hackersvilla776@gmail.com>
2023-03-20 09:38:42 +0530

    Going to my branch

--- cricfit/lib/main.dart
+++ cricfit/lib/main.dart
@@ -3,10 +3,7 @@ import 'package:cricfit/signUp.dart';
 import 'package:cricfit/verifyaccount.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-<<<<<<< HEAD
-import 'Screens/SplashScreen/splash.dart';
-=======
->>>>>>> origin/master
+import 'SplashScreen/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,21 +16,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
         title: 'CricFit',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           fontFamily: GoogleFonts.montserrat().fontFamily,
         ),
         home: const Splash());
-=======
-      title: 'CricFit',
-      theme: ThemeData(
-        fontFamily: GoogleFonts.poppins().fontFamily,
-        primarySwatch: Colors.blue,
-      ),
-      home: verifyaccount(),
-    );
->>>>>>> origin/master
   }
 }
